f889105759052d49a7d142704dc3914131b078b5
Vishal Sharma <vishal2033@gmail.com>
2015-05-29 15:53:12 +0530

    a1.R plotting subjectwise

--- app1/a1.R
+++ app1/a1.R
@@ -6,19 +6,17 @@ sales_prod <- merge(sales, prod, by.x="productId", by.y="id")
 sales_prod_region <- merge(sales_prod, region, by.x="regionId", by.y="id")
 attach(sales_prod_region)
 sales_prod_region  <- sales_prod_region[order(timestamp),]
-auto <- sales_prod_region[(sales_prod_region$category == 'Automobile'),]
-elec <- sales_prod_region[(sales_prod_region$category == 'Electronics'),]
-appliance <- sales_prod_region[(sales_prod_region$category == 'Applicance'),]
-cloths <- sales_prod_region[(sales_prod_region$category == 'Cloths'),]
+
+category <- sales_prod_region[(sales_prod_region$category == 'Automobile'),]
+type <- sales_prod_region[(sales_prod_region$type == 'TV'),]
+brand <- sales_prod_region[(sales_prod_region$brand == 'Honda'),]
+model <- sales_prod_region[(sales_prod_region$model == 'Acer2'),]
 
 rgn <- sales_prod_region[(sales_prod_region$region == 'South'),]
 state <- sales_prod_region[(sales_prod_region$state == 'UP'),]
-city <- sales_prod_region[(sales_prod_region$city == 'Jaipur'),]
+city <- sales_prod_region[(sales_prod_region$city == 'Chennai'),]
 
-apple <- sales_prod_region[(sales_prod_region$brand == 'Honda'),]
-
-
-sub <- city
+sub <- type
 
 sub$month = substring(sub$timestamp,6,7)
 sub$year = substring(sub$timestamp,1,4)
