d9ebd9daeedf4b239b5e7d93e8fbdd856b09604b
Vishal Sharma <vishal2033@gmail.com>
2015-07-16 20:04:36 +0530

    forecast with min max adjustments

--- app2/forecast.js
+++ app2/forecast.js
@@ -14,18 +14,18 @@ function onRResponse(err, res){
 
 var args = {
 	data:[
-		{key : 'Jan-14', value : 4},
+		{key : 'Jan-14', value : 11},
 		{key : 'Feb-14', value : 5},
-		{key : 'Mar-14', value : 6},
-		{key : 'Apr-14', value : 7},
-		{key : 'May-14', value : 4},
-		{key : 'Jun-14', value : 2},
-		{key : 'Jul-14', value : 3},
-		{key : 'Aug-14', value : 5},
-		{key : 'Sep-14', value : 6},
-		{key : 'Oct-14', value : 7},
-		{key : 'Nov-14', value : 11},
-		{key : 'Dec-14', value : 8}
+		{key : 'Mar-14', value : 5},
+		{key : 'Apr-14', value : 2},
+		{key : 'May-14', value : 12},
+		{key : 'Jun-14', value : 5},
+		{key : 'Jul-14', value : 19},
+		{key : 'Aug-14', value : 14},
+		{key : 'Sep-14', value : 17},
+		{key : 'Oct-14', value : 24},
+		{key : 'Nov-14', value : 27},
+		{key : 'Dec-14', value : 18}
 	],
 	forecastPeriod : 1,
 	startYear : 2014,
